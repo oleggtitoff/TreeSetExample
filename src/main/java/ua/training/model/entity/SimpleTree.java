d05858be41ff79c624377b73089f2d94ba535aec
Oleg Titov <oleggtitoff@gmail.com>
2019-12-19 00:24:31 +0200

    In SimpleTree added Tree interface implementation (without methods implementation).

--- src/main/java/ua/training/model/entity/SimpleTree.java
+++ src/main/java/ua/training/model/entity/SimpleTree.java
@@ -1,9 +1,10 @@
 package ua.training.model.entity;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-class SimpleTree<E> {
+class SimpleTree<E> implements Tree<E> {
     private Leaf<E> root;
     private List<E> list;
     private int size = 0;
@@ -11,6 +12,31 @@ class SimpleTree<E> {
     public SimpleTree() {
         list = new LinkedList<>();
         root = new Leaf<>(null);
+    }
+
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    @Override
+    public List<E> get() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Leaf find(E e) {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
     }
 
     class Leaf<E> implements Comparable<E> {
