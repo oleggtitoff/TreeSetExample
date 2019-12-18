5a74183eac297b8881ef1c52bff579413d0179d5
Oleg Titov <oleggtitoff@gmail.com>
2019-12-18 23:58:24 +0200

    In SimpleTree added inner class Leaf.

--- src/main/java/ua/training/SimpleTree.java
+++ src/main/java/ua/training/SimpleTree.java
@@ -1,4 +1,10 @@
 package ua.training;
 
 public class SimpleTree {
+    private class Leaf<E> {
+        private Leaf<E> right;
+        private Leaf<E> left;
+        private E element;
+
+    }
 }
