2d52f481e414977457948c77e55ed6ed14cc10a0
Oleg Titov <oleggtitoff@gmail.com>
2019-12-19 00:19:31 +0200

    In SimpleTree added constructor.

--- src/main/java/ua/training/model/entity/SimpleTree.java
+++ src/main/java/ua/training/model/entity/SimpleTree.java
@@ -1,6 +1,18 @@
 package ua.training.model.entity;
 
-class SimpleTree {
+import java.util.LinkedList;
+import java.util.List;
+
+class SimpleTree<E> {
+    private Leaf<E> root;
+    private List<E> list;
+    private int size = 0;
+
+    public SimpleTree() {
+        list = new LinkedList<>();
+        root = new Leaf<>(null);
+    }
+
     class Leaf<E> implements Comparable<E> {
         private Leaf<E> right;
         private Leaf<E> left;
